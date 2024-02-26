cf905bbc4ed4e06e895c97b01fda9aff2c100c0e
tarunhejmadi <tarunhejmadi@gmail.com>
2024-02-26 16:40:49 -0600

    Intermediate Rewrite
    Doesnt work, some wrong stuff with J. Using new GD scheme

--- Main.py
+++ Main.py
@@ -18,17 +18,50 @@ import numpy as np
 import Entities as ent
 import Functions as fc
 import Plots as plot
-import time as time
 
 if __name__ == '__main__':
-    num_runs = 20
-    J_optimized = np.zeros(num_runs)
+    is_multi = False
+    num_iter = 3
+    J_optimized = np.zeros(num_iter)
     
     # States in [r_x, r_y, v_x, v_y].T format
-    start = time.time()
-    # Call Kalman Filter
-    J_optimized = fc.MultiMonteCarlo(num_runs)
-    end = time.time()
-    print("Total time = ", end - start)
+
+    # Create Instances of robots, 1 of type 1, 1 of type 2
+    robots = []
+    for k in range(1):
+        robots.append(ent.Robot(k))
+        robots.append(ent.Robot(k+1))
+    robots[0].number_robots = len(robots)
+
+    # Create Instances of sensors, 1 of type 1
+    sensors = []
+    for k in range(1):
+        sensors.append(ent.Sensor(k))
+    sensors[0].number_sensors = len(sensors)
+    
+    # Create Instance of Optimization()
+    optimize = ent.Optimization()
+
+    # Create Instance of RNG()
+    if is_multi: rng_class = ent.RNG()
+    
+    # Outer most loop - Optimizes Policy
+    if is_multi:
+        for k in range(num_iter):
+            J_optimized[k] = fc.MultiMonteCarlo(robots, sensors, optimize, rng_class)
+            
+    else:
+        for k in range(num_iter):
+            print("Iter k =", k+1)
+            J_optimized[k] = fc.MonteCarlo(robots, sensors, optimize, is_frozen = False)
+            for t in range(optimize.T-1):
+                print("Timestep: ", t+1)
+                optimize.FreezePolicy(t)
+                fc.MonteCarlo(robots, sensors, optimize, is_frozen = True)
+                optimize.UpdatePartialJ(t)
+                print()
+            fc.GradientDescent(optimize)
+            optimize.Reset()
+            
     # Call plots
-    plot.OptimizedCost(num_runs, J_optimized)
+    plot.OptimizedCost(num_iter, J_optimized)
